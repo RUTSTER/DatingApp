0602f557e7661f4a251cb5f5656491c858f16fe1
Maksim Andruhin <maks.andruhin@gmail.com>
2024-02-13 22:48:38 +0100

    [back] add simple Users controller

---
+++ API/API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _db;
+
+    public UsersController(DataContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _db.Users.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _db.Users.Find(id);
+    }
+}
