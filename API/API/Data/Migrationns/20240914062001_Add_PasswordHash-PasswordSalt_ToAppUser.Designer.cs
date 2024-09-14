29d05b4dafa3604787cd3c3c4b2b72746f004899
Maksim Andruhin <maks.andruhin@gmail.com>
2024-09-14 09:30:06 +0300

    [back] update AppUser, migrations and SQLite table
    add PasswordHash and PasswordSalt fields/columns

--- API/API/Data/Migrationns/20240914062001_Add_PasswordHash-PasswordSalt_ToAppUser.Designer.cs
+++ API/API/Data/Migrationns/20240914062001_Add_PasswordHash-PasswordSalt_ToAppUser.Designer.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrationns
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240914062001_Add_PasswordHash-PasswordSalt_ToAppUser")]
+    partial class Add_PasswordHashPasswordSalt_ToAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");
